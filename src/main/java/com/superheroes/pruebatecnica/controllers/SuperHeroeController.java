a421a16237089e0a72319e1e502ea476b0f44775
Felix Antonio Sirit Murillo <siritfelix@ibm.com>
2021-03-08 20:56:11 -0300

    Agregando EndPoint restantes

--- src/main/java/com/superheroes/pruebatecnica/controllers/SuperHeroeController.java
+++ src/main/java/com/superheroes/pruebatecnica/controllers/SuperHeroeController.java
@@ -1,12 +1,17 @@
 package com.superheroes.pruebatecnica.controllers;
 
+import com.superheroes.pruebatecnica.dto.SuperHeroeDTO;
+import com.superheroes.pruebatecnica.repository.model.SuperHeroe;
 import com.superheroes.pruebatecnica.services.SuperHeroeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +24,8 @@ public class SuperHeroeController {
     public static final String LISTAR = "/listar-todos";
     public static final String BUSCAR_POR_ID = "/buscar-por-id/{id}";
     public static final String BUSCAR_POR_NOMBRE = "/buscar-por-nombre/{nombre}";
+    public static final String MODIFICAR = "/modificar";
+    public static final String ELIMINAR = "/eliminar";
     @Autowired
     private SuperHeroeService superHeroeService;
 
@@ -42,6 +49,25 @@ public class SuperHeroeController {
 
         if (!superHeroeService.ListarPorCoincidenciaTodos(nombre).isEmpty())
             return new ResponseEntity<>(superHeroeService.ListarPorCoincidenciaTodos(nombre), HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Superheroe no encontrado", HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping(MODIFICAR)
+    public ResponseEntity<?> Modificar(@RequestBody SuperHeroeDTO superHeroeDTO) {
+
+        SuperHeroe superHeroe = new SuperHeroe(superHeroeDTO.getId(), superHeroeDTO.getNombre());
+        if (superHeroeService.Modificar(superHeroe).isPresent())
+            return new ResponseEntity<>(superHeroeDTO, HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Superheroe no encontrado", HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping(ELIMINAR)
+    public ResponseEntity<?> Eliminar(@RequestParam Integer id) {
+
+        if (superHeroeService.Eliminar(id))
+            return new ResponseEntity<>("Superheroe Eliminado", HttpStatus.OK);
         else
             return new ResponseEntity<>("Superheroe no encontrado", HttpStatus.NOT_FOUND);
     }
