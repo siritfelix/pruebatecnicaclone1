0df9a943d04122e5396727c3acf8f25ca8d008e8
Felix Antonio Sirit Murillo <siritfelix@ibm.com>
2021-03-09 21:22:53 -0300

    Agregando readme y repositorio remoto

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO superheroes (nombre) VALUES('superman');
-INSERT INTO superheroes (nombre) VALUES('spiderman');
-INSERT INTO superheroes (nombre) VALUES('acuamman');
-INSERT INTO superheroes (nombre) VALUES('batman');
-INSERT INTO superheroes (nombre) VALUES('thor');
-INSERT INTO superheroes (nombre) VALUES('capitan america');
